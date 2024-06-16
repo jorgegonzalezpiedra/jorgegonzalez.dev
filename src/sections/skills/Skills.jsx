0d223d8986bb6c0a0b75ef0a4bc2cd967ce9da7c
Jorge Gonzalez <jorge.gonzalez.piedra@gmail.com>
2024-06-16 15:18:40 +0200

    minnor changes

--- src/sections/skills/Skills.jsx
+++ src/sections/skills/Skills.jsx
@@ -6,22 +6,20 @@ const Skills = () => {
   const words = ["Java", "Spring", "Javascript", "Html", "Css", "Sql"]; //
 
   return (
-    <>
-      <section className="flex">
-        <div className="md:w-3/5 w-full flex items-center justify-center">
-          <OrbitingCirclesDemo />
-        </div>
-        <div className="md:w-3/5 hidden md:flex items-center justify-left">
-          <div className="h-[40rem] flex justify-center items-center px-4">
-            <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-              Construyamos algo chulo con
-              <br />
-              <FlipWords words={words} duration={1000} />
-            </div>
+    <section className="flex">
+      <div className="md:w-3/5 w-full flex items-center justify-center">
+        <OrbitingCirclesDemo />
+      </div>
+      <div className="md:w-3/5 hidden md:flex items-center justify-left">
+        <div className="h-[40rem] flex justify-center items-center px-4">
+          <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+            Construyamos algo chulo con
+            <br />
+            <FlipWords words={words} duration={1000} />
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
