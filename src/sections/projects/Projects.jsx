3eb81c7dc64a270262818c45c476100d72ff2ccb
jorgegonzalezpiedra <jorge.gonzalez.piedra@gmail.com>
2025-02-18 18:51:54 +0100

    No code changes made

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -1,6 +1,7 @@
 import { WobbleCard } from "../../components/ui/wobble-card";
 import personagen from "../../assets/personagen.jpg";
 import sonsofcadia from "../../assets/sonsofcadia.png";
+import tipotest from "../../assets/tipotest.png";
 
 const Projects = () => {
   return (
@@ -12,6 +13,62 @@ const Projects = () => {
         <h1 className="text-7xl font-medium">Proyectos</h1>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full mt-10">
+        <WobbleCard
+          containerClassName="col-span-1 lg:col-span-3 h-full bg-red-700 min-h-[500px] lg:min-h-[300px] bg-white dark:border-white dark:bg-gray-800 border-black border-2"
+          className=""
+          link={"https://sonsofcadia.vercel.app/"}
+        >
+          <div className="max-w-xs">
+            <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] dark:text-white">
+              TipoTest
+            </h2>
+            <p className="mt-4 max-w-[26rem] text-left  text-base/6 dark:text-white">
+              Mi proyecto mas grande.
+            </p>
+            <p className="mt-4 text-left  text-base/6 dark:text-white">
+              TipoTest es una aplicación web pensada para ayudar a la gente a
+              prepar exámenes, opsiciones, y, en general a estudiar con un
+              enfoque basado en la realización de exámenes. TipoTest genera
+              exámenes tipo test a partir de cualquier temario lo  
+              que hace más fácil la preparación de exámenes con temarios
+              pecualiares o de conocimientos muy específicos.
+            </p>
+          </div>
+          <img
+            src={tipotest}
+            width={700}
+            height={600}
+            alt="linear demo"
+            className="absolute -right-4 lg:-right-[2%] filter -bottom-10 object-contain rounded-2xl"
+          />
+        </WobbleCard>
+        <WobbleCard
+          containerClassName="col-span-1 lg:col-span-3 h-full bg-red-700 min-h-[500px] lg:min-h-[300px] bg-white dark:border-white dark:bg-gray-800 border-black border-2"
+          className=""
+          link={"https://sonsofcadia.vercel.app/"}
+        >
+          <div className="max-w-xs">
+            <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] dark:text-white">
+              Sons of Cadia
+            </h2>
+            <p className="mt-4 text-left  text-base/6 dark:text-white">
+              Rediseño de página web para la asociación Sons of Cadia ubicada en
+              Pinto, Madrid.
+            </p>
+            <p className="mt-4 text-left  text-base/6 dark:text-white">
+              La pagina fue rediseñada por mi, pasando de una página web hecha
+              en WordPress a una aplicacion web dinámica hecha con React,
+              Tailwind y Supabase que cuenta con usuarios y sistema de reservas.
+            </p>
+          </div>
+          <img
+            src={sonsofcadia}
+            width={700}
+            height={600}
+            alt="linear demo"
+            className="absolute -right-4 lg:-right-[2%] filter -bottom-10 object-contain rounded-2xl"
+          />
+        </WobbleCard>
         <WobbleCard
           containerClassName="col-span-1 lg:col-span-2 h-full bg-white dark:border-white dark:bg-gray-800 border-black border-2 min-h-[500px] lg:min-h-[300px]"
           className=""
@@ -50,34 +107,6 @@ const Projects = () => {
           <p className="mt-4 max-w-[26rem] text-left  text-base/6 dark:text-white">
             Hecha con HTML, CSS y Javascript
           </p>
-        </WobbleCard>
-        <WobbleCard
-          containerClassName="col-span-1 lg:col-span-3 h-full bg-red-700 min-h-[500px] lg:min-h-[300px] bg-white dark:border-white dark:bg-gray-800 border-black border-2"
-          className=""
-          link={"https://sonsofcadia.vercel.app/"}
-        >
-          <div className="max-w-xs">
-            <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] dark:text-white">
-              Sons of Cadia
-            </h2>
-            <p className="mt-4 text-left  text-base/6 dark:text-white">
-              Rediseño de página web para la asociación Sons of Cadia ubicada en
-              Pinto, Madrid.
-            </p>
-            <p className="mt-4 text-left  text-base/6 dark:text-white">
-              La pagina fue rediseñada por mi, pasando de una página web hecha
-              en WordPress a una aplicacion web dinámica hecha con React, Tailwind y
-              Supabase que cuenta con usuarios y sistema de
-              reservas.
-            </p>
-          </div>
-          <img
-            src={sonsofcadia}
-            width={700}
-            height={600}
-            alt="linear demo"
-            className="absolute -right-4 lg:-right-[2%] filter -bottom-10 object-contain rounded-2xl"
-          />
         </WobbleCard>
       </div>
     </section>
